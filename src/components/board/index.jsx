d3c64101f0a0be51b9b00073cc99e4df8a481b06
TigrannMkrtchyan <tigran.mkrtchyan9999@gmail.com>
2022-09-13 16:24:20 +0400

    init

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -1,16 +1,14 @@
-// import { useEffect, useState } from "react";
-// import Button from "../button";
+import { useState } from "react";
+import Button from "../button";
 import SongButton from "../songButton";
 
 import styles from "./board.module.scss";
 
 const Board = ({ songs }) => {
-  // const [sound, setSound] = useState(new Audio(songs[0]));
-  // const [speed, setSpeed] = useState(1);
-
-  // useEffect(() => {
-  //   sound.playbackRate = speed;
-  // }, [speed, sound]);
+  const [speed, setSpeed] = useState(1);
+  const [interrupt, setInterrupt] = useState(true);
+  const [paused, setPaused] = useState(true);
+  const [played, setPlayed] = useState(false);
 
   const backColors = (arg) => {
     const colors = ["red", "green", "yellow", "blue", "black"];
@@ -29,34 +27,28 @@ const Board = ({ songs }) => {
     return colors[res];
   };
 
-  // const playSound = () => {
-  //   sound.play();
-  // };
-  // const pauseSound = () => {
-  //   sound.pause();
-  // };
+  const playSound = () => {
+    setPlayed(!played);
+  };
+  const pauseSound = () => {
+    setPaused(!paused);
+  };
 
-  // const interruptSound = () => {
-  //   sound.pause();
-  //   sound.currentTime = 0;
-  // };
+  const interruptSound = () => {
+    setInterrupt(!interrupt);
+  };
 
-  // const changeSpeed = (divison) => {
-  //   let res = speed;
-  //   if (divison && speed !== 0.5) {
-  //     res = speed / 2;
-  //   }
-  //   if (!divison && speed !== 2) {
-  //     res = speed * 2;
-  //   }
+  const changeSpeed = (divison) => {
+    let res = speed;
+    if (divison && speed !== 0.5) {
+      res = speed / 2;
+    }
+    if (!divison && speed !== 2) {
+      res = speed * 2;
+    }
 
-  //   setSpeed(res);
-  // };
-
-  // const changeSound = (arg) => {
-  //   interruptSound();
-  //   setSound(new Audio(arg));
-  // };
+    setSpeed(res);
+  };
 
   return (
     <div>
@@ -64,20 +56,23 @@ const Board = ({ songs }) => {
         <div className={styles.mainComponent}>
           {songs.map((value, index) => (
             <SongButton
+              interrupt={interrupt}
               song={value}
-              // onClick={() => changeSound(value)}
+              speed={speed}
               color={backColors(index)}
+              paused={paused}
+              played={played}
             />
           ))}
         </div>
       </div>
-      {/* <div className={styles.component}>
+      <div className={styles.component}>
         <Button onClick={playSound} text={"play"} />
         <Button onClick={pauseSound} text={"pause"} />
         <Button onClick={() => changeSpeed(true)} text={"slow down"} />
         <Button onClick={() => changeSpeed(false)} text={"speed up"} />
         <Button onClick={interruptSound} text={"interrupt"} />
-      </div> */}
+      </div>
     </div>
   );
 };
--- src/components/songButton/index.jsx
+++ src/components/songButton/index.jsx
@@ -1,13 +1,43 @@
+import { useEffect, useState } from "react";
 import styles from "./songButton.module.scss";
 
-const SongButton = ({ color, song }) => {
-  const sound = new Audio(song);
+const SongButton = ({ speed, color, song, interrupt, paused, played }) => {
+  const [sound] = useState(new Audio(song));
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    sound.playbackRate = speed;
+  }, [speed, sound]);
+
+  useEffect(() => {
+    interruptSound();
+  }, [interrupt]);
+
+  useEffect(() => {
+    if (playing) {
+      sound.pause();
+    }
+  }, [paused]);
+
+  useEffect(() => {
+    // sound.ended()
+    // sound.ontransitionend()
+    // sound.onplaying()
+    // sound.onended()
+    // sound.duration()
+
+    if (playing) {
+      sound.play();
+    }
+  }, [played]);
 
   const playSound = () => {
     sound.play();
+    setPlaying(true);
   };
 
   const interruptSound = () => {
+    setPlaying(false);
     sound.pause();
     sound.currentTime = 0;
   };
